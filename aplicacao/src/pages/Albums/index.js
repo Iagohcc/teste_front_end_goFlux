17e33e6ccd8a7c44512cd49cd3361350cdf2a9b0
viniciusurgtec28 <46465161+Iagohcc@users.noreply.github.com>
2021-10-04 13:00:20 -0300

    ajustes consumir api

--- aplicacao/src/pages/Albums/index.js
+++ aplicacao/src/pages/Albums/index.js
@@ -1,26 +1,14 @@
 // import CardAlbum from "../../components/CardAlbum";
 import React, { useEffect, useState } from "react";
-import api from "./services/api";
 
 export default function Albums(){
-  const [albums, setAlbums] = useState();
-
-  useEffect(() => {
-    api
-      .get("/")
-      .then((response) => setUser(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-
   return (
     <div>
       <div className="home_content">
           <div className="text">Albums</div> 
           {/* <CardAlbum /> */}
-          <p>Music: {albums?.}</p>
-          <p>Artist: {albums?.}</p>
+          {/* <p>Music: {albums?.}</p>
+          <p>Artist: {albums?.}</p> */}
       </div>  
     </div>
   );
--- aplicacao/src/pages/Timeline/index.js
+++ aplicacao/src/pages/Timeline/index.js
@@ -1,9 +1,28 @@
-export default function Timeline(){
+import { useEffect, useState } from "react";
+
+export default function Timeline() {
+  const [timeline, setTimeline] = useState();
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "text/xml");
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow',
+      mode: 'no-cors'
+    };
+
+    fetch("http://api.music-story.com/en/news/search?source=Music%20Story", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  })
   return (
     <div>
       <div className="home_content">
-          <div className="text">Timeline</div> 
-      </div>  
+        <div className="text">Timeline</div>
+      </div>
     </div>
   );
 }
